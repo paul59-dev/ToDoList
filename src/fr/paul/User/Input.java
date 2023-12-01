0d544ae647fd1912f2d1ff2459e8c755e7e09983
paul59-dev <paulallebee@gmail.com>
2023-12-01 17:31:40 +0100

    feat(question): request a question

--- src/fr/paul/User/Input.java
+++ src/fr/paul/User/Input.java
@@ -2,12 +2,7 @@ package fr.paul.User;
 
 public class Input {
 	
-	private String question = "Que voulez vous faire ?";
 	private Menu menu = new Menu();
-	
-	public String getQuestion() {
-		return question;
-	}
 	
 	public Menu getMenu() {
 		return menu;
